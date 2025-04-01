73f184ae87a8564eed5218f460c54090195ba7f8
Pericles Maikon <155267900+periclesmaikon@users.noreply.github.com>
2025-04-01 14:51:36 -0300

    Ativando Autenticação

--- meu-app/src/telas/autenticacao.js
+++ meu-app/src/telas/autenticacao.js
@@ -8,10 +8,10 @@ function useVerificarAutenticacao() {
   useEffect(() => {
     const cpf = localStorage.getItem('cpf'); // Verifica se o CPF está no localStorage
 
-    // Se não encontrar o CPF, redireciona para a página de login
-    /* if (!cpf) {
+    //Se não encontrar o CPF, redireciona para a página de login
+    if (!cpf) {
       navigate('/');
-    } */
+    }
   }, [navigate]); // Dependência de `navigate` para redirecionamento
 }
 
